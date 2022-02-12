2e21c64d4cbca3abb21413a0c22644f2a5f4236d
unknown <jirenerose@gmail.com>
2022-02-12 23:57:02 +0530

    work on delete feature

--- src/App.js
+++ src/App.js
@@ -3,16 +3,36 @@ class App extends Component {
     state = {task_arr:[]}
     add_task = () => {
       let new_task = document.getElementById("newtask").value
-      var temp = this.state.task_arr;
+      var temp =  [...this.state.task_arr]
       temp.push(new_task);
       this.setState({task_arr: temp})
     } 
+    display_task = () => {
+      document.getElementById()
+    }
+    delete_task = (id) => {
+      var temp = [...this.state.task_arr];
+      temp.filter(ele => ele !== id)
+      alert(temp)
+      this.setState({task_arr:temp})
+    }
     render(){
       return (
         <Fragment>
           <input type="text" placeholder="Enter new task" id="newtask"></input>
           <button class="btn" onClick={this.add_task} >Add</button>
-          <h1>{this.state.task_arr}</h1>
+          <br></br>
+          <ul>
+            {this.state.task_arr.map(item => {
+                return(
+                  <li>{item}
+                  <button onclick={this.delete_task(item)}>Del</button>
+                  </li>
+                  
+                )
+              }
+            )}
+          </ul>
         </Fragment>
       )
     }
